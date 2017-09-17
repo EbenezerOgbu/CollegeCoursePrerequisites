89b1bff946e0e8ca1d885598707bbf4021d20548
Chinedu Ogbu <ogbuce2006@yahoo.com>
2017-09-17 04:06:40 +0100

    Commit number 4A: Having completed the domain model layer, I have shift my focus to the Service layer of the application. In this commit I have included the interfaces of the two services I will be implementing in this layer.

---
+++ CollegeCoursePrerequisites.Service/ICourseManager.cs
@@ -0,0 +1,14 @@
+﻿using CollegeCoursePrerequisites.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeCoursePrerequisites.Service
+{
+    public interface ICourseManager
+    {
+        string[] PlanCoursesWrapper(string[] courses, string[][] courseDependencies);
+    }
+}
---
+++ CollegeCoursePrerequisites.Service/IPrepareCoursesForInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeCoursePrerequisites.Service
+{
+    public interface IPrepareCoursesForInput
+    {
+        void PrepareCourses(string[] courses);
+        string[] GetCourseList();
+        string[][] GetAllCoursesAndTheirPrerequisites();
+    }
+}
