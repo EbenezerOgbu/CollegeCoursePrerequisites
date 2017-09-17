2c38a941d8dfc5cc5728ea2e0c530f5490ad2d14
Chinedu Ogbu <ogbuce2006@yahoo.com>
2017-09-17 06:25:54 +0100

    Commit number 5: This commits contain the final working application.

---
+++ CollegeCoursePrerequisites.ConsoleApp/CourseManagementModule.cs
@@ -0,0 +1,19 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollegeCoursePrerequisites.Service;
+
+namespace CollegeCoursePrerequisites.ConsoleApp
+{
+    public class CourseManagementModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IPrepareCoursesForInput>().To<PrepareCoursesForInput>();
+            Bind<ICourseManager>().To<CourseManager>();
+        }
+    }
+}
--- CollegeCoursePrerequisites.ConsoleApp/Program.cs
+++ CollegeCoursePrerequisites.ConsoleApp/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CollegeCoursePrerequisites.Service;
+using Ninject;
 
 namespace CollegeCoursePrerequisites.ConsoleApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            IKernel kernal = new StandardKernel(new CourseManagementModule());
+
+            //These are the valid inputs
+
+            string[] testCourses = { "Introduction to Paper Airplanes: ",
+                                     "Advanced Throwing Techniques: Introduction to Paper Airplanes",
+                                     "History of Cubicle Siege Engines: Rubber Band Catapults 101",
+                                     "Advanced Office Warfare: History of Cubicle Siege Engines",
+                                     "Rubber Band Catapults 101: ",
+                                     "Paper Jet Engines: Introduction to Paper Airplanes" };
+
+            //These are the invalid inputs
+
+
+            //string[] testCourses = {
+            //    "Intro to Arguing on the Internet: Godwin’s Law",
+            //    "Understanding Circular Logic: Intro to Arguing on the Internet",
+            //    "Godwin’s Law: Understanding Circular Logic"};
+
+            var prepareCoursesForInput = kernal.Get<PrepareCoursesForInput>();
+            var courseManager = kernal.Get<CourseManager>();
+
+
+            prepareCoursesForInput.PrepareCourses(testCourses);
+
+            string[] courses = prepareCoursesForInput.GetCourseList();
+
+            string[][] courseDependencies = prepareCoursesForInput.GetAllCoursesAndTheirPrerequisites();
+
+            string[] courseOrder = courseManager.PlanCoursesWrapper(courses, courseDependencies);
+
+            if (courseOrder == null)
+            {
+                Console.WriteLine("Circular Dependencies.");
+                Console.ReadKey();
+            }
+            else
+            {
+                foreach (string s in courseOrder)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
