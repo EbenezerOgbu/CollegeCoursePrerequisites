7307f6a92d8f5e2570154c0a904eb05d7659d92e
Chinedu Ogbu <ogbuce2006@yahoo.com>
2017-09-17 03:30:21 +0100

    Commit number 3B: In this commit I have implemented the CourseDependencyGraph entity, and the tests associated with the entity's methods have also passed.

---
+++ CollegeCoursePrerequisites.DomainModel/CourseDependencyGraph.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CollegeCoursePrerequisites.DomainModel
+{
+    public class CourseDependencyGraph
+    {
+        private IList<Course> _nodes;
+        private Hashtable _map;
+
+        public CourseDependencyGraph()
+        {
+            _nodes = new List<Course>();
+            _map = new Hashtable();
+        }
+
+        public Course CreateNewOrGetExistingNode(string name)
+        {
+            if (!_map.ContainsKey(name))
+            {
+                Course node = new Course(name);
+                _nodes.Add(node);
+                _map.Add(name, node);
+            }
+
+            return (Course)_map[name];
+        }
+
+        public void AddEdge(string startName, string endName)
+        {
+            Course start = CreateNewOrGetExistingNode(startName);
+            Course end = CreateNewOrGetExistingNode(endName);
+
+            start.AddCourseNeighbour(end);
+        }
+
+        public IEnumerable<Course> GetNodes()
+        {
+            return _nodes;
+        }
+    }
+}
--- CollegeCoursePrerequisites.UnitTests/CourseDependencyGraphTests.cs
+++ CollegeCoursePrerequisites.UnitTests/CourseDependencyGraphTests.cs
@@ -1,10 +1,6 @@
 ﻿using CollegeCoursePrerequisites.DomainModel;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CollegeCoursePrerequisites.UnitTests
 {
