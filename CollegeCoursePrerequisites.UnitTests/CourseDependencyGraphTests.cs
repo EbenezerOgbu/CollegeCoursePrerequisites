f6fb5a663ada6511d8ec3ec8f489864ada61ffeb
Chinedu Ogbu <ogbuce2006@yahoo.com>
2017-09-17 03:13:19 +0100

    Commit number 3A: Going forward with the analysis, I have identified another domain entity, the CourseDependencyGraph, that I will need to implement. This commit contains the failing tests I have written for the methods I have envisioned for the entity.

---
+++ CollegeCoursePrerequisites.UnitTests/CourseDependencyGraphTests.cs
@@ -0,0 +1,39 @@
+﻿using CollegeCoursePrerequisites.DomainModel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeCoursePrerequisites.UnitTests
+{
+    [TestFixture]
+    public class CourseDependencyGraphTests
+    {
+        [Test]
+        public void CreateNewOrGetExistingNode_WhenCalled_ChangesCourseNodes()
+        {
+            CourseDependencyGraph courseGraph = CreateCourseDependencyGraph();
+            Course course = courseGraph.CreateNewOrGetExistingNode(GetName());
+            Assert.AreEqual(1, courseGraph.GetNodes().Count());
+        }
+
+        [Test]
+        public void GetNodes_ByDefault_ReturnsAnEmptyArray()
+        {
+            CourseDependencyGraph courseGraph = CreateCourseDependencyGraph();
+            var courses = courseGraph.GetNodes();
+            Assert.AreEqual(0, courses.Count());
+        }
+
+        private static CourseDependencyGraph CreateCourseDependencyGraph()
+        {
+            return new CourseDependencyGraph();
+        }
+        private static string GetName()
+        {
+            return "myCourseName";
+        }
+    }
+}
